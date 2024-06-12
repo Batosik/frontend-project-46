fd95206f30dea46d9fe2d19fc7ce49822e214513
Erokhin <8batos@gmail.com>
2024-06-12 14:00:10 +0300

    climate

--- genDiff.js
+++ genDiff.js
@@ -1,8 +1,8 @@
-import _ from 'lodash';
 import * as fs from 'node:fs';
 import * as path from 'node:path';
 import parse from './src/parser.js';
 import diffOutput from './src/formaters/index.js';
+import findDiff from './src/findDiff.js';
 
 const getData = (filePath) => {
   const absPath = path.resolve(process.cwd(), filePath);
@@ -11,40 +11,25 @@ const getData = (filePath) => {
 
 const getFormat = (filepath) => path.extname(filepath).slice(1);
 
-const prepareData = (filepath) => {
-  const data = getData(filepath);
-  const format = getFormat(filepath);
-  return parse(data, format);
-};
-
-const compareObjects = (branch1, branch2) => {
-  const keys = _.keys({ ...branch1, ...branch2 });
-  const sortedKeys = _.sortBy(_.uniq(keys));
-  const result = sortedKeys.map((key) => {
-    if (!Object.hasOwn(branch1, key) && Object.hasOwn(branch2, key)) {
-      return { type: 'add', key, value: branch2[key] };
-    } if (Object.hasOwn(branch2, key)) {
-      if (_.isObject(branch1[key]) && _.isObject(branch2[key])) {
-        return { type: 'nested', key, value: compareObjects(branch1[key], branch2[key]) };
-      } if (_.isEqual(branch1[key], branch2[key])) {
-        return { type: 'equal', key, value: branch1[key] };
-      }
-      return {
-        type: 'changed', key, oldValue: branch1[key], value: branch2[key],
-      };
-    }
-    return { type: 'removed', key, value: branch1[key] };
-  });
-  return result;
-};
+// const prepareData = (filepath) => {
+//   const data = getData(filepath);
+//   const format = getFormat(filepath);
+//   return parse(data, format);
+// }
 
 const genDiff = (file1path, file2path, fromatName) => {
-  const obj1 = prepareData(file1path);
-  const obj2 = prepareData(file2path);
-  const diff = compareObjects(obj1, obj2);
+  const data1 = getData(file1path);
+  const format1 = getFormat(file1path);
+  const obj1 = parse(data1, format1);
+  const data2 = getData(file2path);
+  const format2 = getFormat(file2path);
+  const obj2 = parse(data2, format2);
+  // const obj1 = prepareData(file1path);
+  // const obj2 = prepareData(file2path);
+  const diff = findDiff(obj1, obj2);
   return diffOutput(diff, fromatName);
 };
 
-console.log(genDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml', 'json'))
+console.log(genDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml', 'json'));
 
 export default genDiff;
---
+++ src/findDiff.js
@@ -0,0 +1,24 @@
+import _ from 'lodash';
+
+const findDiff = (branch1, branch2) => {
+  const keys = _.keys({ ...branch1, ...branch2 });
+  const sortedKeys = _.sortBy(_.uniq(keys));
+  const result = sortedKeys.map((key) => {
+    if (!Object.hasOwn(branch1, key) && Object.hasOwn(branch2, key)) {
+      return { type: 'add', key, value: branch2[key] };
+    } if (Object.hasOwn(branch2, key)) {
+      if (_.isObject(branch1[key]) && _.isObject(branch2[key])) {
+        return { type: 'nested', key, value: findDiff(branch1[key], branch2[key]) };
+      } if (_.isEqual(branch1[key], branch2[key])) {
+        return { type: 'equal', key, value: branch1[key] };
+      }
+      return {
+        type: 'changed', key, oldValue: branch1[key], value: branch2[key],
+      };
+    }
+    return { type: 'removed', key, value: branch1[key] };
+  });
+  return result;
+};
+
+export default findDiff;
