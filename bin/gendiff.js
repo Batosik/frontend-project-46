be556feb363526cac46af43cf8b14cf8047e9d1c
Erokhin <8batos@gmail.com>
2024-07-14 02:08:30 +0300

    set

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,7 @@ import genDiff from '../src/index.js';
 const program = new Command();
 
 program
+  .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .helpOption('-h, --help', 'output usage information')
